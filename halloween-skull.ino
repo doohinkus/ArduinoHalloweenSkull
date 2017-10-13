cb87ae1200ee886655cf3a6b282e249bca55837c
R. Perez <clickpass@gmail.com>
2017-10-13 14:34:06 -0700

    Update halloween-skull.ino

--- halloween-skull.ino
+++ halloween-skull.ino
@@ -3,19 +3,18 @@
 Servo myServo;
 
 //set up pins for devices
-const int trigPin = 2;
-const int echoPin = 7;
+const int trigPin = 7;
 const int servo = 9;
 
-//sets the amount of spin on the servo
+//sets the amount of spin/sweep range on the servo
 const int sweepRange = 90;
 
 //variables for distnace calculations
-int pos = 0;
+long cm;
 long duration;
-int distance;
 
-//sets the range of the sensor in centimeters
+
+//sets the target range of the sensor in centimeters--how far away something is to trigger servo
 int range = 70;
 
 //sweep servo function
@@ -38,7 +37,6 @@ void restServo(){
 void setup() {
   // set up inputs and outputs
   pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
   
   //This is for serial output feedback, you can fine tune distance setting with this info
   Serial.begin(9600);
@@ -48,24 +46,37 @@ void setup() {
 
 }
 
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
 void loop() {
-  //check distance
+  //check distance by creating pulses
+  pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(5);
   digitalWrite(trigPin, LOW);
   
-  //calcucate distance
-  duration = pulseIn(echoPin,HIGH);
-  distance = duration*.034/2;
+  // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+  //four pin models have a separate pins for this, which makes better sense. 
+  pinMode(trigPin, INPUT);
+  duration = pulseIn(trigPin, HIGH);
+  
+  cm = microsecondsToCentimeters(duration);
   
   //output distance, change the range of the sensor in the variables at the top
   Serial.print("Distance: ");
-  Serial.println(distance);
+  Serial.println(cm);
   
   //trigger servo sweep when in range
-  if (distance<range){
+  if (cm < range){
      sweepServo();     
   }else{
   //reset servo when out of range--this is kind of cool, it makes the skull seem more lively
