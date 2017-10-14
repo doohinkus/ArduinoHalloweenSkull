67268b72a1b6cfad7cd8c84dee3b6f4fb221f1c0
R. Perez <clickpass@gmail.com>
2017-10-13 22:56:52 -0700

    Update halloween-skull.ino

--- halloween-skull.ino
+++ halloween-skull.ino
@@ -47,10 +47,10 @@ void setup() {
 }
 
 long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The speed of sound is 340 m/s or 34 microseconds per centimeter.
   // The ping travels out and back, so to find the distance of the object we
   // take half of the distance travelled.
-  return microseconds / 29 / 2;
+  return microseconds / 34 / 2;
 }
 
 void loop() {
